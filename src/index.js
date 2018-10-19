2e357bc1f7038894c044046e69b884294ea3bd26
thomasvds <vanderstraeten.thomas@gmail.com>
2018-10-19 11:03:10 +0200

    Try/catch on errors

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
 const { logger } = require('./helpers/logger');
 const { handleMessage } = require('./messageHandler');
 
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
   const { body } = event.Records[0];
   logger.info(`Received message: ${body}`);
   const message = JSON.parse(body);
-  await handleMessage(message);
-  return {};
-  // event.Records.forEach((record) => {
-
-      // .then(() => {
-      //   logger.info(`Success handling message`);
-      //   // By default if Lambda terminates gracefully, SQS deletes the message.
-      //   // Note that callback is of the form (err, res), so if we pass null
-      //   // as first argument it is considered a success.
-      //   callback(null, 'success');
-      // })
-      // .catch((err) => {
-      //   if (err.name === 'InvalidMessageError') {
-      //     logger.error(err.toString());
-      //     // Delete the message since it is invalid.
-      //     callback(null, err.toString());
-      //   } else {
-      //     // Fail so the message gets re-enqueued, by passing a non-null first
-      //     // argument in the callback.
-      //     logger.error(err.toString());
-      //     callback(err.toString());
-      //   }
-      // });
-  // });
+  try {
+    await handleMessage(message);
+    // By default if Lambda terminates gracefully, SQS deletes the message.
+    // Note that callback is of the form (err, res), so if we pass null
+    // as first argument it is considered a success.
+    logger.info('Successfully processed message');
+    callback(null, 'success');
+  } catch(error) {
+    if (err.name === 'InvalidMessageError') {
+      logger.error(err.toString());
+      // Delete the message since it is invalid.
+      callback(null, err.toString());
+    } else {
+      // Fail so the message gets re-enqueued, by passing a non-null first
+      // argument in the callback.
+      logger.error(err.toString());
+      callback(err.toString());
+    }
+  }
 };
